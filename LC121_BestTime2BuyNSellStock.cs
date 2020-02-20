555abedf83661b2b796e44981eb065062f383d06
leocui9387 <leocui9387@gmail.com>
2020-02-20 17:52:59 -0500

    Add files via upload

---
+++ LC121_BestTime2BuyNSellStock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algos
+{
+    class LC121_BestTime2BuyNSellStock
+    {
+        public static void runner()
+        {
+            Random rand = new Random();
+
+            int len = 0;
+            for (int i = 0; i < rand.Next(1,20); i++)
+            {
+                Console.WriteLine(rand.Next(1, 99));
+            }
+
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+
+
+
+
+
+
+
+            return 0;
+        }
+
+
+
+    }
+}
