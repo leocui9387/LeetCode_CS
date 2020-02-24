3d38dc96ada8b2e789b462e15e4ab4a7a9bec65a
Leo Cui <Basic.Bear@outlook.com>
2020-02-23 20:09:18 -0500

    Quick Sort with Duplicates up and running.

--- LC121_BestTime2BuyNSellStock.cs
+++ LC121_BestTime2BuyNSellStock.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace LeetCode_CS
 {
@@ -15,7 +15,7 @@ namespace LeetCode_CS
             int[] price_array = new int[len];
             for (int i = 0; i < len; i++)
             {
-                price_array[i] = rand.Next(1, 5);
+                price_array[i] = rand.Next(1, 20);
                 Console.WriteLine("Time:" + i + "|Price:" + price_array[i]);
 
             }
@@ -31,7 +31,7 @@ namespace LeetCode_CS
         {
             QuickSort<int>(prices, 0, prices.Length - 1);
 
-            Dictionary<int, int> price_time = new Dictionary<int, int>();
+             Lookup<int, int> price_time = prices.ToLookup(price => );
 
             for (int i = 0; i < prices.Length; i++)
             {
@@ -53,37 +53,37 @@ namespace LeetCode_CS
 
             int init_position = p_beg;
             int dupz = 0;
-            int i;
-
             
-
-            for (i = p_beg + 1; i <= p_end; i++)
+            for (int k = p_beg + 1; k <= p_end; k++)
             {
-                if (p_InArray[p_beg].CompareTo(p_InArray[i]) > 0)
+                
+                if (p_InArray[p_beg].CompareTo(p_InArray[k]) > 0)
+                {
                     init_position++;
-
-                if (p_InArray[p_beg].CompareTo(p_InArray[i]) == 0)
+                }
+                    
+                if (p_InArray[p_beg].CompareTo(p_InArray[k]) == 0)
                     dupz++;
-                Console.WriteLine("index:" +i+ "|init:" + init_position + "|dupz:" + dupz);
+                //Console.WriteLine("index(" + k + "): " + p_InArray[k] + "|init:" + init_position + "|dupz:" + dupz);
             }
 
-            Swapper<T>(p_InArray[p_beg], p_InArray[p_beg + init_position]);
-
+            Swapper<T>(p_InArray, p_beg, init_position);
+                        
             // normal quick sort
-            i = p_beg;
+            int i = p_beg;
             int j = p_end;
             int dup_index = 1;
 
             while ((i + dupz) < j)
             {
                 // find LEFT
-                while (p_InArray[i].CompareTo(p_InArray[init_position]) < 1)
+                while (p_InArray[i].CompareTo(p_InArray[init_position]) < 1 && i < init_position)
                 {
-                    Console.WriteLine("item " + i + ":" + p_InArray[i] +"|init position:" + init_position + "|dup index" + dup_index );
+                    //Console.WriteLine("item " + i + ":" + p_InArray[i] +"|init position:" + init_position + "|dup index" + dup_index );
                     if (p_InArray[i].CompareTo(p_InArray[init_position]) == 0)
                     {
 
-                        Swapper<T>(p_InArray[i], p_InArray[init_position + dup_index]);
+                        Swapper<T>(p_InArray,i, init_position + dup_index);
                         dup_index++;
                         continue;
                     }
@@ -92,40 +92,44 @@ namespace LeetCode_CS
                     
                 
                 // find RIGHT
-                while (p_InArray[j].CompareTo(p_InArray[init_position]) > -1)
+                while (p_InArray[j].CompareTo(p_InArray[init_position]) > -1 && j > (init_position + dupz))
                 {
                     if (p_InArray[j].CompareTo(p_InArray[init_position]) == 0)
                     {
-                        Swapper<T>(p_InArray[j], p_InArray[init_position + dup_index]);
+                        Swapper<T>(p_InArray,j, init_position + dup_index);
                         dup_index++;
                         continue;
                     }
                     j--;
                 }
-                    
                 
-                
-                //deal with dup
-                
-                
-
                 //swap
                 if ((i + dupz) < j){
-                    Swapper<T>(p_InArray[j], p_InArray[i]);
+                    Swapper<T>(p_InArray, j, i);
+                }
+
+                // recurse
+                if(init_position != p_beg)
+                {
+                    QuickSort<T>(p_InArray, p_beg, init_position -1);
+                }
+                if (init_position + dupz != p_end)
+                {
+                    QuickSort<T>(p_InArray, init_position + dupz + 1 , p_end);
                 }
 
                 //safety
-                Console.WriteLine("LEO");
-
+                //Console.WriteLine("LEO");
+                
             }
 
 
         }
 
-        private static void Swapper<T>(T p_one, T p_two){
-            T buffer = p_one;
-            p_one = p_two;
-            p_two = buffer;
+        private static void Swapper<T>(T[] p_array, int p_one, int p_two){
+            T buffer = p_array[ p_one];
+            p_array[p_one] = p_array[p_two];
+            p_array[p_two] = buffer;
         }
 
     }
---
+++ LC12_Int2Roman.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_CS
+{
+    class LC12_Int2Roman
+    {
+
+        public static void runner()
+        {
+            /*
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine(rand.Next(1, 3999));
+            }
+            */
+
+            String[] testCases = { "I", "MMMCCLXXVIII", "MMMDCCCXV", "MMMCCCLXXXII", "MMMDCXXIV", "MDCCXXXVII", "LXXXII", "MCDXXIX", "MMMCCCLXIV", "MDCII", "DCCXLI" };
+            int[] testCases_int = { 1, 3278, 3815, 3382, 3624, 1737, 82, 1429, 3364, 1602, 741 };
+
+            foreach (int tCase in testCases_int)
+            {
+                Console.WriteLine(LC12_Int2Roman.IntToRoman(tCase));
+            }
+
+        }
+
+        public static string IntToRoman(int num)
+        {
+
+            int output = 0;
+            Dictionary<int, Char> RNmap = new Dictionary<int, char>();
+            RNmap.Add(1, 'I');
+            RNmap.Add(5,'V');
+            RNmap.Add(10,'X');
+            RNmap.Add(50, 'L');
+            RNmap.Add(100, 'C');
+            RNmap.Add(500, 'D');
+            RNmap.Add(1000, 'M');
+
+
+
+            return "leo";
+        }
+
+        public static int RomanToInt(string s)
+        {
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("New String: " + s);
+
+            int output = 0;
+            Dictionary<Char, int> RNmap = new Dictionary<char, int>();
+
+            RNmap.Add('I', 1);
+            RNmap.Add('V', 5);
+            RNmap.Add('X', 10);
+            RNmap.Add('L', 50);
+            RNmap.Add('C', 100);
+            RNmap.Add('D', 500);
+            RNmap.Add('M', 1000);
+
+            for (int i = s.Length - 1; i > -1; i--)
+            {
+                if (i + 1 < s.Length && RNmap[s[i + 1]] > RNmap[s[i]])
+                {
+                    output -= RNmap[s[i]];
+                }
+                else
+                {
+                    output += RNmap[s[i]];
+                }
+
+                Console.WriteLine("Total:" + output + "|new dig:" + s[i]);
+            }
+
+            Console.WriteLine("-------------------------------------------------------");
+
+            return output;
+        }
+
+    }
+
+
+
+}
+
