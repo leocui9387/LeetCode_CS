2bd6d0a4ea4cf9bf6c445a4a99e75a3bdf9ef947
Leo Cui <Basic.Bear@outlook.com>
2020-03-24 20:08:15 -0400

    before massive changes

--- Medium/LC0011_ContainerMostWater.cs
+++ Medium/LC0011_ContainerMostWater.cs
@@ -38,6 +38,90 @@ namespace LeetCode_CS.Medium
             int hL = height.Length;
             int areaBuff;
             // max potential 
+
+            Dictionary<int, int> potential_AreaIndex = new Dictionary<int, int>();
+            Dictionary<int, int> retention_AreaIndex = new Dictionary<int, int>();
+
+            
+            for (int i = hL - 1, j=0; i > -1; i--, j++)
+            {
+                
+                areaBuff = -i * height[i];
+                if (!potential_AreaIndex.ContainsKey(areaBuff)) potential_AreaIndex.Add(areaBuff, i);
+
+                areaBuff = -(hL - 1 - j) * height[j];
+                if (!retention_AreaIndex.ContainsKey(areaBuff)) retention_AreaIndex.Add(areaBuff, j);
+            }
+
+            SortedList<int, int> pAI = new SortedList<int, int>(potential_AreaIndex);
+            SortedList<int, int> rAI = new SortedList<int, int>(retention_AreaIndex);
+            // max retention
+            /*
+            for (int i = 0; i < hL; i++)
+            {
+                areaBuff = -(hL - 1 - i) * height[i];
+                if (!retention_AreaIndex.ContainsKey(areaBuff)) retention_AreaIndex.Add(areaBuff, i);
+
+            }
+            */
+
+            Console.WriteLine("POTENTIAL");
+
+            foreach (KeyValuePair<int, int> kvp in pAI)
+            {
+                Console.Write("KEY:" + kvp.Key + "|VAL:" + kvp.Value);
+                Console.WriteLine();
+            }
+            Console.WriteLine("RETENTION");
+            foreach (KeyValuePair<int, int> kvp in rAI)
+            {
+                Console.Write("KEY:" + kvp.Key + "|VAL:" + kvp.Value);
+                Console.WriteLine();
+            }
+
+            // iterate through retention/potential based on which next step has the highest value
+
+            int max = 0;
+
+            int r = 0, p = 0;
+            int rIndex, pIndex;
+
+            
+            while (-max > pAI.Keys[p] && -max > rAI.Keys[r])
+            {
+                rIndex = rAI.Values[r];
+                pIndex = pAI.Values[p];
+
+                areaBuff = Math.Min(height[rIndex], height[pIndex]) * Math.Abs(pIndex - rIndex);
+
+                Console.WriteLine("p:" + p + "|r:" + r + "|area:" + areaBuff);
+                if (max < areaBuff) max = areaBuff;
+
+                if (rAI.Values[r + 1] < rAI.Values[r]) r++;
+                else if (rAI.Keys[r] < pAI.Keys[p]) r++;
+                else p++;
+
+                Console.WriteLine("p:" + p + "|r:" + r + "|area:" + areaBuff);
+
+            }
+
+            
+
+            return max;
+        }
+
+        public static int MaxArea_0(int[] height)
+        {
+
+            /* 
+             * calculate max potential
+             * calculate min loss
+             * 
+             */
+
+            int hL = height.Length;
+            int areaBuff;
+            // max potential 
             int[] pArea = new int[hL];
             Dictionary<int, int> potential_AreaIndex = new Dictionary<int, int>();
             for (int i = hL-1; i > -1; i--) 
