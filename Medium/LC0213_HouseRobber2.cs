6155ec81ee4ce500d789cf5e233b69b273832ab6
Leo Cui <Basic.Bear@outlook.com>
2020-05-17 01:31:47 -0400

    LC0213 finished

--- Medium/LC0213_HouseRobber2.cs
+++ Medium/LC0213_HouseRobber2.cs
@@ -10,10 +10,32 @@ namespace LeetCode_CS.Medium
         {
             // forward prop - 
 
+            int forward = origRob(nums);
+            Array.Reverse(nums);
+            int back = origRob(nums);
 
 
+            return Math.Max(forward, back);
+        }
 
-            return 0;
+        private int origRob(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
+
+            int[] nMemo = new int[nums.Length];
+
+            nMemo[0] = nums[0];
+            nMemo[1] = Math.Max(nums[1], nums[0]);
+
+            int tLength = nums.Length;
+            for (int i = 2; i < tLength; i++)
+            {
+                if (i == 2 && (nums[i] + nMemo[i - 2] > nMemo[i - 1])) tLength--;
+                nMemo[i] = Math.Max(nums[i] + nMemo[i - 2], nMemo[i - 1]);
+            }
+
+            return nMemo[tLength - 1];
         }
     }
 }
